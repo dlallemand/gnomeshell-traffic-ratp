fb49a64a0bebafa71bfa6d1246ec2915bb6ea097
Didier LALLEMAND <dlallemand@cityway.fr>
2016-12-21 14:43:19 +0100

    suppression fichier exemples + ajout script vscode

---
+++ .vscode/build-task.sh
@@ -0,0 +1,19 @@
+# Simple bash script to build the Gnome Shell extension
+
+CURRENT_DIR=$(pwd)
+echo -n "Compile settings.."
+glib-compile-schemas ./trafficRATP@dlallemand/schemas && echo 'ok' || echo 'failed'
+
+echo -n "Compile translation..."
+for i in $(find . -name "*.po"); do 
+    input="$i"; 
+    output="${i%.po}.mo"; 
+    #echo "msgfmt -v $input -o $output"; 
+    msgfmt -v $input -o $output > /dev/null 2>&1 
+done && echo 'ok' || echo 'failed'
+
+
+echo -n "Install extension..."
+cp -R * ~/.local/share/gnome-shell/extensions && echo 'ok' || echo 'failed'
+
+echo "Done."
